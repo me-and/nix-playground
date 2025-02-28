b5140317aa153575a6bb1f9bd9eb4b9ae1d6a147
Adam Dinwoodie <adam@dinwoodie.org>
2025-02-28 21:37:07 +0000

    Build Git targets on arm

--- default.nix
+++ default.nix
@@ -1,3 +1,1 @@
-let
-  pkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/20c41e7373b5c4c2517f8a810757f526c4e0cdac.tar.gz") {};
-in pkgs.python3Packages.cfn-lint
+import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/273172a3a8569cd0bb9eae45f3ab77df352c72f5.tar.gz") {}
