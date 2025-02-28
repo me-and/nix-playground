57231f37941759562f179c139ce660c9c9f4112e
Adam Dinwoodie <adam@dinwoodie.org>
2025-02-28 21:26:20 +0000

    git bisect start git bisect bad 273172a3a8569cd0bb9eae45f3ab77df352c72f5 git bisect good 3a05eebede89661660945da1f151959900903b6a git bisect good 15c23f7d88e92159150ce6325a41627d40d13350 git bisect good 7ebf23d4f9e0c3f4c97227877e712000887e6365 git bisect bad db568a7a146372a65475febf916f3025d4422dd9

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
 let
-  pkgs = import <nixpkgs> {};
-in pkgs.hello
+  pkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/20c41e7373b5c4c2517f8a810757f526c4e0cdac.tar.gz") {};
+in pkgs.python3Packages.cfn-lint
